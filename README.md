3d90a8e31de1704c8d9c815e17f2181e07a4f70d
Oleg-programer <olegantonenko63@gmail.com>
2020-11-09 19:21:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-max3ny-wals7i
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-max3ny-wals7i)
---
+++ src/app/processors/processors.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Processor } from '../processor';
+import { PROCESSORS } from '../mock-processors';
+
+@Component({
+  selector: 'app-processors',
+  templateUrl: './processors.component.html',
+  styleUrls: ['./processors.component.css']
+})
+
+export class ProcessorsComponent implements OnInit {
+
+  processors = PROCESSORS;
+  selectedProcessor: Processor;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(processor: Processor): void {
+    this.selectedProcessor = processor;
+  }
+}
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
