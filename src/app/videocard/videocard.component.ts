ed6d5b6a3c17102aee316582ea5004d39b05134c
Oleg-programer <olegantonenko63@gmail.com>
2020-11-10 18:40:23 +0200

    1 file modified

--- src/app/videocard/videocard.component.ts
+++ src/app/videocard/videocard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Videocard } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { VIDEOCARDS } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +10,7 @@ import { HEROES } from '../mock-heroes';
 
 export class VideocardesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes = VIDEOCARDS;
   selectedVideocard: Videocard;
 
   constructor() { }
