f1fa8f86fb7fd0f9755d92bba843ed1e3f9bfe16
Oleg-programer <olegantonenko63@gmail.com>
2020-11-10 19:06:30 +0200

    1 file modified

---
+++ src/app/power/power.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Box } from '../box';
+import { boxes } from '../mock-boxes';
+
+@Component({
+  selector: 'app-boxes',
+  templateUrl: './boxes.component.html',
+  styleUrls: ['./boxes.component.css']
+})
+
+export class BoxesComponent implements OnInit {
+
+  boxes = boxes;
+  selectedBox: Box;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(box: Box): void {
+    this.selectedBox = box;
+  }
+}
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
