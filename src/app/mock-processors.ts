be1df0297701ff2c727579d25a561a22fb512757
Oleg-programer <olegantonenko63@gmail.com>
2020-11-11 19:14:50 +0200

    1 file modified

--- src/app/mock-processors.ts
+++ src/app/mock-processors.ts
@@ -1,16 +1,16 @@
 import { Processor } from "./processor";
 
 export const PROCESSORS: Processor[] = [
-  { id: 11, name: "Dr 2", socket: "AM4", power: 12 },
-  { id: 12, name: "Narco", socket: "AM4", power: 12 },
-  { id: 13, name: "Bombasto", socket: "AM4", power: 12 },
-  { id: 14, name: "Celeritas", socket: "AM4", power: 12 },
-  { id: 15, name: "Magneta", socket: "AM4", power: 12 },
-  { id: 16, name: "RubberMan", socket: "AM4", power: 12 },
-  { id: 17, name: "Dynama", socket: "AM4", power: 12 },
-  { id: 18, name: "Dr IQ", socket: "AM4", power: 12 },
-  { id: 19, name: "Magma", socket: "AM4", power: 12 },
-  { id: 20, name: "Tornado", socket: "AM4", power: 12 }
+  { id: 11, name: "Intel 1", socket: "AM4", output: 12 },
+  { id: 12, name: "Intel 2", socket: "AM4", output: 12 },
+  { id: 13, name: "Bombasto", socket: "AM4", output: 12 },
+  { id: 14, name: "Celeritas", socket: "AM4", output: 12 },
+  { id: 15, name: "Magneta", socket: "AM4", output: 12 },
+  { id: 16, name: "RubberMan", socket: "AM4", output: 12 },
+  { id: 17, name: "Dynama", socket: "AM4", output: 12 },
+  { id: 18, name: "Dr IQ", socket: "AM4", output: 12 },
+  { id: 19, name: "Magma", socket: "AM4", output: 12 },
+  { id: 20, name: "Tornado", socket: "AM4", output: 12 }
 ];
 
 /*
