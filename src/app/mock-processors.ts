8b8928e441d6d9361c283bd0c266fe7e5607d955
Oleg-programer <olegantonenko63@gmail.com>
2020-11-11 19:11:27 +0200

    1 file modified

--- src/app/mock-processors.ts
+++ src/app/mock-processors.ts
@@ -2,15 +2,15 @@ import { Processor } from "./processor";
 
 export const PROCESSORS: Processor[] = [
   { id: 11, name: "Dr 2", socket: "AM4", power: 12 },
-  { id: 12, name: "Narco" },
-  { id: 13, name: "Bombasto" },
-  { id: 14, name: "Celeritas" },
-  { id: 15, name: "Magneta" },
-  { id: 16, name: "RubberMan" },
-  { id: 17, name: "Dynama" },
-  { id: 18, name: "Dr IQ" },
-  { id: 19, name: "Magma" },
-  { id: 20, name: "Tornado" }
+  { id: 12, name: "Narco", socket: "AM4", power: 12 },
+  { id: 13, name: "Bombasto", socket: "AM4", power: 12 },
+  { id: 14, name: "Celeritas", socket: "AM4", power: 12 },
+  { id: 15, name: "Magneta", socket: "AM4", power: 12 },
+  { id: 16, name: "RubberMan", socket: "AM4", power: 12 },
+  { id: 17, name: "Dynama", socket: "AM4", power: 12 },
+  { id: 18, name: "Dr IQ", socket: "AM4", power: 12 },
+  { id: 19, name: "Magma", socket: "AM4", power: 12 },
+  { id: 20, name: "Tornado", socket: "AM4", power: 12 }
 ];
 
 /*
