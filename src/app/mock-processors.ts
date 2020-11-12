a1c75633881cdcdf83262d8d02f12030a381857f
Oleg-programer <olegantonenko63@gmail.com>
2020-11-12 09:41:23 +0200

    ещё пара проциков

--- src/app/mock-processors.ts
+++ src/app/mock-processors.ts
@@ -6,7 +6,7 @@ export const PROCESSORS: Processor[] = [
   { id: 13, name: "AMD Ryzen 7 5800X 3.8GHz", socket: "AM4", output: 105 },
   { id: 14, name: "AMD Ryzen 5 3500X 3.6GHz", socket: "AM4", output: 65 },
   { id: 15, name: "AMD Ryzen 3 3100 3.6GHz", socket: "AM4", output: 65 },
-  { id: 16, name: "RubberMan", socket: "AM4", output: 12 },
+  { id: 16, name: "Intel Core i3-9100F 3.6GHz", socket: "1151", output: 65 },
   { id: 17, name: "Dynama", socket: "AM4", output: 12 },
   { id: 18, name: "Dr IQ", socket: "AM4", output: 12 },
   { id: 19, name: "Magma", socket: "AM4", output: 12 },
