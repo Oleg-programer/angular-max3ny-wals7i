06250d9e5bda2cf2bf7ffd350249c3d66951d689
Oleg-programer <olegantonenko63@gmail.com>
2020-11-12 09:37:48 +0200

    1 file modified

--- src/app/mock-processors.ts
+++ src/app/mock-processors.ts
@@ -2,10 +2,10 @@ import { Processor } from "./processor";
 
 export const PROCESSORS: Processor[] = [
   { id: 11, name: "AMD Ryzen 9 5950X 3.4GHz", socket: "AM4", output: 105 },
-  { id: 12, name: "Intel 2", socket: "AM4", output: 12 },
-  { id: 13, name: "Bombasto", socket: "AM4", output: 12 },
-  { id: 14, name: "Celeritas", socket: "AM4", output: 12 },
-  { id: 15, name: "Magneta", socket: "AM4", output: 12 },
+  { id: 12, name: "Intel Core i5-10400F 2.9GHz", socket: "1200", output: 65 },
+  { id: 13, name: "AMD Ryzen 7 5800X 3.8GHz", socket: "AM4", output: 105 },
+  { id: 14, name: "AMD Ryzen 5 3500X 3.6GHz", socket: "AM4", output: 65 },
+  { id: 15, name: "AMD Ryzen 3 3100 3.6GHz", socket: "AM4", output: 65 },
   { id: 16, name: "RubberMan", socket: "AM4", output: 12 },
   { id: 17, name: "Dynama", socket: "AM4", output: 12 },
   { id: 18, name: "Dr IQ", socket: "AM4", output: 12 },
