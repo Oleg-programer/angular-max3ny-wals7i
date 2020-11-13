63818c84c1dafc13b49b4372e03ac34c50560233
Oleg-programer <olegantonenko63@gmail.com>
2020-11-13 19:50:31 +0200

    powers mocks added

--- src/app/mock-powers.ts
+++ src/app/mock-powers.ts
@@ -1,16 +1,16 @@
 import { Processor } from "./processor";
 
 export const PROCESSORS: Processor[] = [
-  { id: 11, name: "Dr 2" },
-  { id: 12, name: "Narco" },
-  { id: 13, name: "Bombasto" },
-  { id: 14, name: "Celeritas" },
-  { id: 15, name: "Magneta" },
-  { id: 16, name: "RubberMan" },
-  { id: 17, name: "Dynama" },
-  { id: 18, name: "Dr IQ" },
-  { id: 19, name: "Magma" },
-  { id: 20, name: "Tornado" }
+  { id: 11, name: "Aerocool VX Plus 600 600W", output: 600 },
+  { id: 12, name: "Intel Core i5-10400F 2.9GHz", socket: "1200", output: 65 },
+  { id: 13, name: "AMD Ryzen 7 5800X 3.8GHz", socket: "AM4", output: 105 },
+  { id: 14, name: "AMD Ryzen 5 3500X 3.6GHz", socket: "AM4", output: 65 },
+  { id: 15, name: "AMD Ryzen 3 3100 3.6GHz", socket: "AM4", output: 65 },
+  { id: 16, name: "Intel Core i3-9100F 3.6GHz", socket: "1151", output: 65 },
+  { id: 17, name: "Intel Core i7-10700K 3.8GHz", socket: "1200", output: 125 },
+  { id: 18, name: "AMD Ryzen 5 2600 3.4GHz", socket: "AM4", output: 65 },
+  { id: 19, name: "Intel Pentium G5600F 3.9GHz", socket: "1151", output: 54 },
+  { id: 20, name: "Intel Celeron G4930 3.2GHz", socket: "1151", output: 54 }
 ];
 
 /*
