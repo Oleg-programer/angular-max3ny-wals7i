74a96a7f98c23c6016ab0498bb2e0a8a70fbba8f
Oleg-programer <olegantonenko63@gmail.com>
2020-11-13 09:20:27 +0200

    harddrive data

--- src/app/mock-harddrives.ts
+++ src/app/mock-harddrives.ts
@@ -1,16 +1,16 @@
-import { Processor } from "./processor";
+import { Harddrive } from "./harddrive";
 
-export const PROCESSORS: Processor[] = [
-  { id: 11, name: "Dr 2" },
-  { id: 12, name: "Narco" },
-  { id: 13, name: "Bombasto" },
-  { id: 14, name: "Celeritas" },
-  { id: 15, name: "Magneta" },
-  { id: 16, name: "RubberMan" },
-  { id: 17, name: "Dynama" },
-  { id: 18, name: "Dr IQ" },
-  { id: 19, name: "Magma" },
-  { id: 20, name: "Tornado" }
+export const PROCESSORS: Harddrive[] = [
+  { id: 11, name: "Western Digital Blue 1TB 7200rpm", slot: "SATA", output: 6.8 },
+  { id: 12, name: "Intel Core i5-10400F 2.9GHz", slot: "1200", output: 65 },
+  { id: 13, name: "AMD Ryzen 7 5800X 3.8GHz", slot: "AM4", output: 105 },
+  { id: 14, name: "AMD Ryzen 5 3500X 3.6GHz", slot: "AM4", output: 65 },
+  { id: 15, name: "AMD Ryzen 3 3100 3.6GHz", slot: "AM4", output: 65 },
+  { id: 16, name: "Intel Core i3-9100F 3.6GHz", slot: "1151", output: 65 },
+  { id: 17, name: "Intel Core i7-10700K 3.8GHz", slot: "1200", output: 125 },
+  { id: 18, name: "AMD Ryzen 5 2600 3.4GHz", slot: "AM4", output: 65 },
+  { id: 19, name: "Intel Pentium G5600F 3.9GHz", slot: "1151", output: 54 },
+  { id: 20, name: "Intel Celeron G4930 3.2GHz", slot: "1151", output: 54 }
 ];
 
 /*
