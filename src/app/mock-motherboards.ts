8753d9b7b8a25611b5f69e481d12b7bed327f6d5
Oleg-programer <olegantonenko63@gmail.com>
2020-11-14 16:57:04 +0200

    1 file modified

--- src/app/mock-motherboards.ts
+++ src/app/mock-motherboards.ts
@@ -7,7 +7,7 @@ export const MOTHERBOARD: Motherboard[] = [
   { id: 24, name: "Asus Prime A320M-K ", socket: "AM4", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
   { id: 25, name: "ASRock A320M-DVS R4.0  ", socket: "AM4", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
   { id: 26, name: "Gigabyte H410M S2", socket: "1151", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
-  { id: 17, name: "Intel Core i7-10700K 3.8GHz", socket: "1200", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
+  { id: 17, name: "MSI B450M-A Pro Max", socket: "1200", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
   { id: 18, name: "AMD Ryzen 5 2600 3.4GHz", socket: "AM4", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
   { id: 19, name: "Intel Pentium G5600F 3.9GHz", socket: "1151", ram: "DDR4", gpu: "PCI", hdd: "SATA" },
   { id: 20, name: "Intel Celeron G4930 3.2GHz", socket: "1151", ram: "DDR4", gpu: "PCI", hdd: "SATA" }
