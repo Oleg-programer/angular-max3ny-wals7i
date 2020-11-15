9a49e8656a56e99d62e230cef1c4242f1f11a729
Oleg-programer <olegantonenko63@gmail.com>
2020-11-15 09:16:16 +0200

    4 files modified

---
+++ src/app/box.ts
@@ -0,0 +1,12 @@
+export interface Power {
+  id: number;
+  name: string;
+  output: number;
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
