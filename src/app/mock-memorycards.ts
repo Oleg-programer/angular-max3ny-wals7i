b966f0a1edfa440de36eec329b35698e9219e4c3
Oleg-programer <olegantonenko63@gmail.com>
2020-11-16 15:11:06 +0200

    1 file modified

--- src/app/mock-memorycards.ts
+++ src/app/mock-memorycards.ts
@@ -4,10 +4,10 @@ export const MEMORYCARDS: Memorycard[] = [
   { id: 51, name: "HyperX", clot: "DDR4" },
   { id: 52, name: "Crucial", clot: "DDR4" },
   { id: 53, name: "Kingston", clot: "DDR4" },
-  { id: 54, name: "Kingston1", clot: "DDR3l" },
+  { id: 54, name: "Kingston SODIMM", clot: "DDR3l" },
   { id: 55, name: "Transcend", clot: "DDR4" },
-  { id: 56, name: "HyperX", clot: "DDR4" },
-  { id: 57, name: "HyperX", clot: "DDR4" },
+  { id: 56, name: "AMD", clot: "DDR4" },
+  { id: 57, name: "AMD SODIMM", clot: "DDR3" },
   { id: 58, name: "HyperX", clot: "DDR4" },
   { id: 59, name: "HyperX", clot: "DDR4" },
   { id: 60, name: "HyperX", clot: "DDR4" }
