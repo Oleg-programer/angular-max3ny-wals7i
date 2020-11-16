5ff7255b353c435f7b143c1067fb03cd9114ffd7
Oleg-programer <olegantonenko63@gmail.com>
2020-11-16 14:42:18 +0200

    1 file modified

--- src/app/mock-processors.ts
+++ src/app/mock-processors.ts
@@ -1,16 +1,16 @@
 import { Processor } from "./processor";
 
 export const PROCESSORS: Processor[] = [
-  { id: 41, name: "AMD Ryzen 9 5950X 3.4GHz", socket: "AM4", output: 105 },
-  { id: 42, name: "Intel Core i5-10400F 2.9GHz", socket: "1200", output: 65 },
-  { id: 43, name: "AMD Ryzen 7 5800X 3.8GHz", socket: "AM4", output: 105 },
-  { id: 44, name: "AMD Ryzen 5 3500X 3.6GHz", socket: "AM4", output: 65 },
-  { id: 45, name: "AMD Ryzen 3 3100 3.6GHz", socket: "AM4", output: 65 },
-  { id: 46, name: "Intel Core i3-9100F 3.6GHz", socket: "1151", output: 65 },
-  { id: 47, name: "Intel Core i7-10700K 3.8GHz", socket: "1200", output: 125 },
-  { id: 48, name: "AMD Ryzen 5 2600 3.4GHz", socket: "AM4", output: 65 },
-  { id: 49, name: "Intel Pentium G5600F 3.9GHz", socket: "1151", output: 54 },
-  { id: 50, name: "Intel Celeron G4930 3.2GHz", socket: "1151", output: 54 }
+  { id: 11, name: "AMD Ryzen 9 5950X 3.4GHz", socket: "AM4", output: 105 },
+  { id: 12, name: "Intel Core i5-10400F 2.9GHz", socket: "1200", output: 65 },
+  { id: 13, name: "AMD Ryzen 7 5800X 3.8GHz", socket: "AM4", output: 105 },
+  { id: 14, name: "AMD Ryzen 5 3500X 3.6GHz", socket: "AM4", output: 65 },
+  { id: 15, name: "AMD Ryzen 3 3100 3.6GHz", socket: "AM4", output: 65 },
+  { id: 16, name: "Intel Core i3-9100F 3.6GHz", socket: "1151", output: 65 },
+  { id: 17, name: "Intel Core i7-10700K 3.8GHz", socket: "1200", output: 125 },
+  { id: 18, name: "AMD Ryzen 5 2600 3.4GHz", socket: "AM4", output: 65 },
+  { id: 19, name: "Intel Pentium G5600F 3.9GHz", socket: "1151", output: 54 },
+  { id: 20, name: "Intel Celeron G4930 3.2GHz", socket: "1151", output: 54 }
 ];
 
 /*
