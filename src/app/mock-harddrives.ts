40639283bade8bbea74a87d2598d38ae5e456194
Oleg-programer <olegantonenko63@gmail.com>
2020-11-16 15:24:06 +0200

    1 file modified

--- src/app/mock-harddrives.ts
+++ src/app/mock-harddrives.ts
@@ -1,16 +1,16 @@
 import { Harddrive } from "./harddrive";
 
 export const PROCESSORS: Harddrive[] = [
-  { id: 11, name: "Western Digital Blue 1TB 7200rpm", slot: "SATA", output: 6.8 },
-  { id: 12, name: "HP Hot Plug SC Midline (MDL) 2TB 7200rpm", slot: "SAS", output: 6.8},
-  { id: 13, name: "Western Digital Blue 1 TB 5400", slot: "SATA", output: 3.3 },
-  { id: 14, name: "Toshiba P300 1TB 7200rpm", slot: "SATA", output: 6.4 },
-  { id: 15, name: "Seagate BarraCuda HDD 1TB 5400rpm", slot: "SATA", output: 1.7 },
-  { id: 16, name: "Western Digital Purple 6TB 64MB 5400rpm", slot: "SATA", output: 5.3 },
-  { id: 17, name: "Toshiba Enterprise Capacity 8TB 7200rpm", slot: "SATA", output: 9.1 },
-  { id: 18, name: "Western Digital Ultrastar DC HC310 3.5 4TB 7200rpm ", slot: "SATA", output: 7 },
-  { id: 19, name: "Western Digital Gold Enterprise Class 18 TB 7200 rpm", slot: "SATA", output: 6.5 },
-  { id: 20, name: "Western Digital Red Plus 4TB 5400rpm", slot: "SATA", output: 4.5 }
+  { id: 61, name: "Western Digital Blue 1TB 7200rpm", slot: "SATA", output: 6.8 },
+  { id: 62, name: "HP Hot Plug SC Midline (MDL) 2TB 7200rpm", slot: "SAS", output: 6.8},
+  { id: 63, name: "Western Digital Blue 1 TB 5400", slot: "SATA", output: 3.3 },
+  { id: 64, name: "Toshiba P300 1TB 7200rpm", slot: "SATA", output: 6.4 },
+  { id: 65, name: "Seagate BarraCuda HDD 1TB 5400rpm", slot: "SATA", output: 1.7 },
+  { id: 66, name: "Western Digital Purple 6TB 64MB 5400rpm", slot: "SATA", output: 5.3 },
+  { id: 67, name: "Toshiba Enterprise Capacity 8TB 7200rpm", slot: "SATA", output: 9.1 },
+  { id: 68, name: "Western Digital Ultrastar DC HC310 3.5 4TB 7200rpm ", slot: "SATA", output: 7 },
+  { id: 69, name: "Western Digital Gold Enterprise Class 18 TB 7200 rpm", slot: "SATA", output: 6.5 },
+  { id: 70, name: "Western Digital Red Plus 4TB 5400rpm", slot: "SATA", output: 4.5 }
 ];
 
 /*
